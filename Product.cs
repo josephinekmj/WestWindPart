e21d9caecef7bf24a8971e49b642212d5be39b86
Josephine Johansen <158573410+josephinekmj@users.noreply.github.com>
2024-05-01 15:34:31 +0200

    Første iteration af projektet
    Product.cs har properties for et Product
    ProductRepository har liste af products, kontruktør og metoder for Product

---
+++ Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindSpareParts
+{
+    public class Product
+    {
+        public string SupplierProductNumber { get; set; }
+        public string ProductName { get; set; }
+        public string SKU { get; set; }
+        public double DefaultUnitPriceDKK { get; set; }
+        public double MarkUpPercentage { get; set; }
+        public string Category {  get; set; } // new eller renovated evt bool
+        public int LeedTimeWeeks { get; set; }
+        public string UnitOfMeasure { get; set; } //kg, units, box
+    
+
+
+
+    
+    }
+}
---
+++ ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindSpareParts
+{
+    public class ProductRepository
+    {
+        private List<Product> products; //liste til at gemme produkterne
+
+        public ProductRepository() 
+        {  
+            products = new List<Product>(); //initialisering af produktlisten
+        } 
+
+        public void CreateProduct(Product product)
+        {             
+            products.Add(product); //det nye produkt tilføjes til listen products
+        }
+        
+        public void DeleteProduct(Product product) 
+        { 
+            products.Remove(product); //sletter et produkt fra listen products
+        }
+
+        public void SearchProduct(Product product)
+        {
+            products.BinarySearch(product); //søger efter et produkt i listen products 
+        }
+          
+        
+
+    }
+}
